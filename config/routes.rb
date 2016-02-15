5b5b8b3461a0a1786b72622c305747207e5b21f5
Andrew Trebuhin <andrewtrebuhin@gmail.com>
2016-02-15 15:25:59 +0200

    Add pagination

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,13 @@ Rails.application.routes.draw do
 
   root 'pages#home'
 
-  resources :recipes
+  resources :recipes do
+    member do
+      post 'like'
+    end
+  end
+
+
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
